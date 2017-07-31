de1ee221e8e46fb933471f98321887b7155ab760
Jesse Caulfield <jesse.caulfield@keybridgeglobal.com>
2017-07-31 12:07:09 -0400

    move Timezone and Empty string converters here

---
+++ src/main/java/ch/keybridge/lib/faces/converter/ConvertTimezone.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2014 Caulfield IP Holdings (Caulfield) and/or its affiliates.
+ *  All rights reserved. Use is subject to license terms.
+ *
+ *  Software Code is protected by Caulfield Copyrights. Caulfield hereby reserves
+ *  all rights in and to Caulfield Copyrights and no license is granted under
+ *  Caulfield Copyrights in this Software License Agreement. Caulfield generally
+ *  licenses Caulfield Copyrights for commercialization pursuant to the terms of
+ *  either Caulfield's Standard Software Source Code License Agreement or
+ *  Caulfield's Standard Product License Agreement.
+ *
+ *  A copy of either License Agreement can be obtained on request by email from:
+ *  info@caufield.org.
+ */
+package ch.keybridge.lib.faces.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ * JSF converter for java.util.TimeZone
+ *
+ * @author Jesse Caulfield
+ */
+@FacesConverter(value = "convertTimezone")
+public class ConvertTimezone implements Converter {
+
+  @Override
+  public Object getAsObject(FacesContext context, UIComponent component, String value) {
+    return java.util.TimeZone.getTimeZone(value);
+  }
+
+  @Override
+  public String getAsString(FacesContext context, UIComponent component, Object value) {
+    if (value instanceof java.util.TimeZone) {
+      return ((java.util.TimeZone) value).getID();
+    }
+    return null;
+  }
+
+}
