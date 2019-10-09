96020c782d169820fc40d0863ad95141249fd66d
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2019-10-09 15:03:15 -0400

    use a trusting http/https client

--- src/main/java/ch/keybridge/lib/faces/validator/LinkValidator.java
+++ src/main/java/ch/keybridge/lib/faces/validator/LinkValidator.java
@@ -18,23 +18,28 @@
  */
 package ch.keybridge.lib.faces.validator;
 
+import java.security.cert.X509Certificate;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.ValidatorException;
+import javax.net.ssl.*;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 /**
  * HTTP link validator. Validates an HTTP link to determine if the link is
- * publicly available.
+ * publicly available. Note: Since v4.1.0 this uses a trusting client; for HTTPS
+ * links this does NOT validate the server certificate.
  * <p>
  * This validator also updates the input component CSS to include a BS-4 status
  * indicator.
  *
  * @author Key Bridge
  * @since v0.6.0 created 01/27/19
+ * @since v4.1.0 use a trusting client to accept all SSL certificates
  */
 @FacesValidator("linkValidator")
 public class LinkValidator extends AbstractValidator {
@@ -63,12 +68,87 @@ public class LinkValidator extends AbstractValidator {
       return;
     }
     try {
-      Response response = ClientBuilder.newClient().target((String) value).request()
-        .header(HttpHeaders.USER_AGENT, MOZILLA)
-        .get();
+//      Response response = ClientBuilder.newClient().target((String) value).request()
+      Response response = trustingClient()
+              .target((String) value)
+              .request()
+              .header(HttpHeaders.USER_AGENT, MOZILLA)
+              .get();
     } catch (Exception e) {
-      setValidityStatus(component, false);
+      setValidityStatus(component, false); // update the jsf component
       throwErrorException("Not available", "This resource is not available. " + e.getMessage());
+    }
+  }
+  /**
+   * From org.glassfish.jersey.client.ClientProperties
+   */
+  private static final String READ_TIMEOUT = "jersey.config.client.readTimeout";
+  /**
+   * From org.glassfish.jersey.client.ClientProperties
+   */
+  private static final String CONNECT_TIMEOUT = "jersey.config.client.connectTimeout";
+
+  /**
+   * 1,000 milliseconds = 1 seconds.
+   * <p>
+   * Connect timeout interval, in milliseconds. The value MUST be an instance
+   * convertible to Integer. A value of zero (0) is equivalent to an interval of
+   * infinity. The default value is infinity (0).
+   */
+  private static final int TIMEOUT_CONNECT = 1000;
+  /**
+   * 1,000 milliseconds = 1 seconds.
+   * <p>
+   * Read timeout interval, in milliseconds. The value MUST be an instance
+   * convertible to Integer. A value of zero (0) is equivalent to an interval of
+   * infinity. The default value is infinity (0).
+   */
+  private static final int TIMEOUT_READ = 1000;
+
+  /**
+   *
+   * @return @throws Exception
+   */
+  protected Client trustingClient() throws Exception {
+    SSLContext sc = SSLContext.getInstance("TLSv1");//Java 8   // NoSuchAlgorithmException
+    System.setProperty("https.protocols", "TLSv1");//Java 8
+    TrustManager[] trustAllCerts = {new InsecureTrustManager()};
+    sc.init(null, trustAllCerts, new java.security.SecureRandom()); // KeyManagementException
+    HostnameVerifier allHostsValid = (String string, SSLSession ssls) -> true;
+    Client client = ClientBuilder.newBuilder().sslContext(sc).hostnameVerifier(allHostsValid).build();
+    client.property(CONNECT_TIMEOUT, TIMEOUT_CONNECT); // should immediately connect
+    client.property(READ_TIMEOUT, TIMEOUT_READ); // wait for processing
+    return client;
+  }
+
+  /**
+   * Manage which X509 certificates may be used to authenticate the remote side
+   * of a secure socket
+   */
+  private class InsecureTrustManager implements X509TrustManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void checkClientTrusted(final X509Certificate[] chain, final String authType) {
+      // Everyone is trusted!
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void checkServerTrusted(final X509Certificate[] chain, final String authType) {
+      // Everyone is trusted!
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+      return new X509Certificate[0];
     }
   }
 }
